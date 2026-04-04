ec36ba581bae0e2446273a8d0855b99b775e25ad
Omowumi Badmus <olaomo31@yahoo.co.uk>
2026-04-04 14:22:50 -0400

    Save progress: 690.1

--- countdown.sh
+++ countdown.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Program that counts down to zero from a given argument
-
+'''That should work. In your for loop, use sleep to make the script pause for 1 second 
+after each number is printed.'''
 if [[ $1 -gt 0 ]]
 then
-  for (( i = $1; i > 0; i--; ))
+  for (( i = $1; i > 0; i--))
   do
     echo $i
-    sleep 1
+    sleep 3
   done
 else
   echo Include a positive integer as the first argument.
