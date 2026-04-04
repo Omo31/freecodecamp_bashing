018aa56af00d83a534ccd4990d4bc2317831e686
Omowumi Badmus <olaomo31@yahoo.co.uk>
2026-04-04 14:35:49 -0400

    Save progress: 696.1

--- countdown.sh
+++ countdown.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # Program that counts down to zero from a given argument
-'''Awesome. Except it should print 0 instead of stopping at 1. Change the condition in your 
-for loop so that it checks for i >= 0.'''
+echo -e "\n~~ Countdown Timer ~~\n"
 if [[ $1 -gt 0 ]]
 then
   for (( i = $1; i >= 0; i--))
