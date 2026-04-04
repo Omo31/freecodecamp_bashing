fec8b7aba3d0c9ddbd535f2080fff04df6c4f1fd
Omowumi Badmus <olaomo31@yahoo.co.uk>
2026-04-04 15:32:56 -0400

    Save progress: 830.1

--- bingo.sh
+++ bingo.sh
@@ -2,5 +2,8 @@
 # Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
+'''Back in your script, use the RANDOM variable to set NUMBER to a 
+random number instead of 5'''
+
 NUMBER=$RANDOM
 echo $NUMBER
