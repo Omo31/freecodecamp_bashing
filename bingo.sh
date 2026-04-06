88acf2bdaebd8371ed53f595afc6175e2df64a0b
Omowumi Badmus <olaomo31@yahoo.co.uk>
2026-04-06 11:38:40 -0400

    Save progress: 950.1

--- bingo.sh
+++ bingo.sh
@@ -2,10 +2,16 @@
 # Bingo Number Generator
 echo -e "\n~~ Bingo Number Generator ~~\n"
 
-'''The RANDOM variable will generate a random number between 0 and 32767. You can use the 
-modulus operator to make it in the range you want. In your script, change the NUMBER variable to $RANDOM%75'''
 
 NUMBER=$(( RANDOM % 75 + 1 ))
-echo $NUMBER
 
 TEXT="The next number is, "
+
+#for (( i = NUMBER ))
+  if (( NUMBER <= 15 ))
+  then
+    echo $TEXT B:$NUMBER
+  fi
+    
+
+
