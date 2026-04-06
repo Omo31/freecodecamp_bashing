5aad4a01d1852262251024c78f9fc0343b9af435
Omowumi Badmus <olaomo31@yahoo.co.uk>
2026-04-06 08:58:55 -0400

    Save progress: 928.1

--- bingo.sh
+++ bingo.sh
@@ -5,5 +5,5 @@ echo -e "\n~~ Bingo Number Generator ~~\n"
 '''The RANDOM variable will generate a random number between 0 and 32767. You can use the 
 modulus operator to make it in the range you want. In your script, change the NUMBER variable to $RANDOM%75'''
 
-NUMBER=$RANDOM%75
+NUMBER=$(( RANDOM % 75 + 1 ))
 echo $NUMBER
