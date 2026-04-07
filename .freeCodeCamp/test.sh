ae17e93176feba96f53e0f8dc530f9269e528c49
Omowumi Badmus <olaomo31@yahoo.co.uk>
2026-04-07 10:26:08 -0400

    Save progress: 1150.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -4,6 +4,7 @@ echo -e "\n~~ Fortune Teller ~~\n"
 
 #RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Dont't count on it" "Ask again later")
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-echo ${RESPONSES[5]}
-N=$(( RANDOM % 6 ))
-echo $N
+
+N=$(( RANDOM %1 ))
+
+echo ${RESPONSES[$N]}
